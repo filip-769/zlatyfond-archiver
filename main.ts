de8ce5d9971b9adedaf767d48ce78f9208de5418
filip-769 <filip769@proton.me>
2024-12-29 16:38:02 +0100

    small fix

--- main.ts
+++ main.ts
@@ -5,6 +5,8 @@ const headers: any = {
     "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:133.0) Gecko/20100101 Firefox/133.0",
 }
 
+Deno.mkdirSync("./files", { recursive: true });
+
 const username = prompt("Enter your username") || "Anonymous";
 
 const list: string[] = [];
